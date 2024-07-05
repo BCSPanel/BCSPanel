d0d8e8d3bd342ac8a18fcaa124e007920e7bb0cd
bddjr <bddjrcn@outlook.com>
2024-07-05 18:10:01 +0800

    优化login

--- src/server/httprouter/login.go
+++ src/server/httprouter/login.go
@@ -10,19 +10,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type loginJson struct {
-	// 安全上下文
-	Secure bool `json:"secure"`
-	// 是否处于注册模式
-	Isregister bool `json:"isregister"`
-	// 用户名
-	Username string `json:"username"`
-	// 密码
-	Password string `json:"password"`
-	// 注册模式发送验证码
-	VerificationCode string `json:"verification_code"`
-}
-
 type apiLogin struct{}
 
 func (a apiLogin) Init(g *gin.RouterGroup) {
@@ -32,18 +19,14 @@ func (a apiLogin) Init(g *gin.RouterGroup) {
 }
 
 func (a apiLogin) InitBasic(loginGroup *gin.RouterGroup) {
+	// 使用Basic登录
 	basiclogin.New(loginGroup, func(ctx *gin.Context, username, password string, secure bool) {
 		cookie, err := user.Login(username, password, secure)
-		if err != nil {
-			// 失败
-			ctx.String(401, err.Error())
-			ctx.Error(err)
-			return
+		if a.loginSetCookie(ctx, cookie, err) {
+			// 成功
+			ctx.Header("Referrer-Policy", "no-referrer")
+			scriptRedirect(ctx, 401, conf.Http.Old_PathPrefix)
 		}
-		// 成功
-		ctx.Writer.Header().Add("Set-Cookie", cookie.String())
-		ctx.Header("Referrer-Policy", "no-referrer")
-		scriptRedirect(ctx, 401, conf.Http.Old_PathPrefix)
 	})
 }
 
@@ -51,12 +34,24 @@ func (a apiLogin) handlerLogin(ctx *gin.Context) {
 	// 退出登录，如果有效
 	mysession.LogOutSessionForRequest(ctx.Request)
 
+	type loginJsonType struct {
+		// 安全上下文
+		Secure bool `json:"secure"`
+		// 是否处于注册模式
+		Isregister bool `json:"isregister"`
+		// 用户名
+		Username string `json:"username"`
+		// 密码
+		Password string `json:"password"`
+		// 注册模式发送验证码
+		VerificationCode string `json:"verification_code"`
+	}
+
 	// 解析表单
-	var loginJson = &loginJson{}
+	loginJson := &loginJsonType{}
 	err := ctx.BindJSON(loginJson)
 	if err != nil {
-		ctx.Status(400)
-		ctx.Writer.WriteString(err.Error())
+		ctx.String(400, err.Error())
 		ctx.Error(err)
 		return
 	}
@@ -70,15 +65,22 @@ func (a apiLogin) handlerLogin(ctx *gin.Context) {
 		cookie, err = user.Login(loginJson.Username, loginJson.Password, loginJson.Secure)
 	}
 
+	if a.loginSetCookie(ctx, cookie, err) {
+		// 成功
+		ctx.Status(200)
+	}
+}
+
+func (a apiLogin) loginSetCookie(ctx *gin.Context, cookie *http.Cookie, err error) (ok bool) {
 	if err != nil {
 		// 失败
 		ctx.String(401, err.Error())
 		ctx.Error(err)
-		return
+		return false
 	}
 	// 成功
 	ctx.Writer.Header().Add("Set-Cookie", cookie.String())
-	ctx.Status(200)
+	return true
 }
 
 func (a apiLogin) handlerLogout(ctx *gin.Context) {
@@ -86,9 +88,12 @@ func (a apiLogin) handlerLogout(ctx *gin.Context) {
 	cookie, ok := mysession.LogOutSessionForRequest(ctx.Request)
 	if ok {
 		// 会话有效，已退出
-		ctx.Header("Set-Cookie", cookie.String())
+		ctx.Writer.Header().Add("Set-Cookie", cookie.String())
+	} else {
+		// 强制退出
+		ctx.SetCookie(mysession.SessionCookieName, "x", -1, "", "", false, true)
 	}
-	// 返回303
+	// 重定向
 	ctx.Redirect(303, conf.Http.Old_PathPrefix+"login/")
 }
 
