5264fe7b428da43d7ea2db762ec0782a5b311992
bddjr <bddjrcn@outlook.com>
2024-11-01 23:25:54 +0800

    optimize main

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/bddjr/BCSPanel/src/mygc"
 	"github.com/bddjr/BCSPanel/src/myinit"
 	"github.com/bddjr/BCSPanel/src/mylog"
-	"github.com/bddjr/BCSPanel/src/myrand"
 	"github.com/bddjr/BCSPanel/src/shutdown"
 )
 
@@ -24,6 +23,14 @@ func main() {
 		}
 	}()
 
+	myinit.Init()
+
+	config.Update()
+	cmdserver.Start()
+	httpserver.Start()
+	go mygc.GC_laterSecond(1)
+
+	// 捕捉停止信号
 	signalCtrlC := make(chan os.Signal, 1)
 	signal.Notify(signalCtrlC,
 		// CTRL+C
@@ -34,17 +41,6 @@ func main() {
 		syscall.SIGHUP,
 		// syscall.SIGTSTP,
 	)
-
-	myinit.Init()
-	mylog.INFOln("main test RandBytes")
-	myrand.RandBytes(1)
-
-	config.Update()
-	cmdserver.Start()
-	httpserver.Start()
-	go mygc.GC_laterSecond(1)
-
-	// 捕捉停止信号
 	<-signalCtrlC
 	shutdown.Shutdown(1)
 }
